e056f780ca45026d823406f81501caf5d097a1f0
happy <happyhaha@g.ncu.edu.tw>
2022-04-26 23:38:28 +0800

    實現基本聊天室功能

--- client.py
+++ client.py
@@ -1,23 +1,48 @@
-import socket, threading, pickle
+import socket, threading, pickle, time
+from xml.etree.ElementTree import TreeBuilder
+
+from numpy import rec
 PORT = 5050
 FORMAT = "utf-8-sig"
 Dis_msg = "disconnecting"
-# SERVER = socket.gethostbyname(socket.gethostname())
-SERVER = "123.241.254.143"
+SERVER = socket.gethostbyname(socket.gethostname())
+# SERVER = "123.241.254.143"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def send(name, send_msg):
-    msg = (name, send_msg)
-    message = pickle.dumps(msg)
-    client.send(message)
+    msg = name + ":" +send_msg
+    client.send(msg.encode(FORMAT))
+
+
+def recive():
+    while True:
+        try:
+            message = client.recv(1024).decode(FORMAT)
+            print(message)
+            if message == "EVERYBODYLOGOUT":
+                client.close()
+                break
+        except:
+            print(f"error occur")
+            client.close()
+            break
+
+def using():
+    while True:
+        time.sleep(0.5)
+        send_msg = input(f"{name}:")
+        send(name, send_msg)
+        if send_msg == "exit":
+            client.close()
+            break
+    send(name, "bye bye")
+
 if __name__ == "__main__":
     client.connect(ADDR)
     name = input("ur name:")
-    while True:
-        send_msg = input("輸入想傳送的msg:")
-        send(name, send_msg)
-        if send_msg == "exit":
-            break
-    send(name, "bye bye")
+    us_thread = threading.Thread(target=using)
+    us_thread.start()
+    re_thread = threading.Thread(target=recive)
+    re_thread.start()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, threading, pickle
+import socket, threading, pickle, time
 # 功能可以接收訊息(基本)，之後要可以進型別的處理(爬蟲、寄信等等)
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -7,28 +7,47 @@ Dis_msg = "disconnecting"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
-FORAMT = "utf-8-sig"
+FORMAT = "utf-8-sig"
 
+peoples_in_room = []
+def announce(conn,):
+    peoples_in_room.append(conn)
+def kickout(conn):
+    for x in peoples_in_room:
+        if x[0] == conn:
+            peoples_in_room.remove(x)
+            break
+    print(peoples_in_room)
+
+def broadcast(message):
+    data = message.encode(FORMAT)
+    for people in peoples_in_room:
+        people.send(data)
+   
 def handle_msg(conn, addr):
     print(f"[NEW CONNECTIONS] {addr} connected")
     connected = True
     while connected:
-        data = b""
-        msg = conn.recv(4096)
-        data = pickle.loads(msg)
-        if  data[1] == "exit":
+        data = conn.recv(1024).decode(FORMAT)
+        broadcast(data)
+        if  data[-4:-1] == "exit":
+            kickout(conn)
             break
         else:
-            print(f"{data[0]} says {data[1]}")
+            print(f"{data}")
     conn.close()
     print(f"DISCONNECTED!!")
+
 def start():
     server.listen()
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_msg, args = (conn, addr))
+        thread = threading.Thread(target = announce, args = (conn, ))
         thread.start()
-        print(f"the number of activing thread is {threading.activeCount() - 1}")
+        thread1 = threading.Thread(target=handle_msg, args = (conn, addr))
+        thread1.start()
+
+        print(f"now we got {threading.activeCount() - 1} in our chatting room.")
 if __name__ == "__main__":
     print("server is started at ", SERVER)
     start()
