cd3685f58c83e2d07dfe6dc858056feec5abee34
happy <happyhaha@g.ncu.edu.tw>
2022-04-27 11:06:58 +0800

    新增正常離開聊天室功能

--- client.py
+++ client.py
@@ -1,15 +1,15 @@
 import socket, threading, pickle, time
-from xml.etree.ElementTree import TreeBuilder
 
-from numpy import rec
 PORT = 5050
 FORMAT = "utf-8-sig"
 Dis_msg = "disconnecting"
 SERVER = socket.gethostbyname(socket.gethostname())
 # SERVER = "123.241.254.143"
 ADDR = (SERVER, PORT)
+flag = True
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 def send(name, send_msg):
     msg = name + ":" +send_msg
@@ -21,28 +21,26 @@ def recive():
         try:
             message = client.recv(1024).decode(FORMAT)
             print(message)
-            if message == "EVERYBODYLOGOUT":
-                client.close()
+            if message == "now shutting all client down":
+                send(name, "exit")
+                break
+            elif message == "exit":
                 break
         except:
             print(f"error occur")
             client.close()
             break
 
-def using():
+if __name__ == "__main__":
+    client.connect(ADDR)
+    name = input("ur name:")
+
+    re_thread = threading.Thread(target=recive, daemon=True)
+    re_thread.start()
     while True:
         time.sleep(0.5)
         send_msg = input(f"{name}:")
         send(name, send_msg)
         if send_msg == "exit":
-            client.close()
-            break
-    send(name, "bye bye")
-
-if __name__ == "__main__":
-    client.connect(ADDR)
-    name = input("ur name:")
-    us_thread = threading.Thread(target=using)
-    us_thread.start()
-    re_thread = threading.Thread(target=recive)
-    re_thread.start()
+            send(name, "bye bye")
+            break    
--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def announce(conn,):
     peoples_in_room.append(conn)
 def kickout(conn):
     for x in peoples_in_room:
-        if x[0] == conn:
+        if x == conn:
             peoples_in_room.remove(x)
             break
     print(peoples_in_room)
@@ -29,27 +29,43 @@ def handle_msg(conn, addr):
     connected = True
     while connected:
         data = conn.recv(1024).decode(FORMAT)
-        broadcast(data)
-        if  data[-4:-1] == "exit":
+        if  data[-4:] == "exit":
+            conn.send("exit".encode(FORMAT))
+            connected = False
+            conn.close()
             kickout(conn)
             break
         else:
+            broadcast(data)
             print(f"{data}")
-    conn.close()
     print(f"DISCONNECTED!!")
-
+def control():
+    while True:
+        message = input("輸入指令")
+        if message == "shutdownclient" and len(peoples_in_room) != 0:
+            broadcast("now shutting all client down")
+            time.sleep(2)
+            print(f"server and clients shutdown")
+        elif len(peoples_in_room) == 0:
+            print("現在沒人")
+        elif message == "listall":
+            for people in peoples_in_room:
+                print(f"{people}")
+        elif message == "shutdownserver":
+            server.close()
 def start():
     server.listen()
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target = announce, args = (conn, ))
         thread.start()
-        thread1 = threading.Thread(target=handle_msg, args = (conn, addr))
+        thread1 = threading.Thread(target = handle_msg, args = (conn, addr))
         thread1.start()
-
-        print(f"now we got {threading.activeCount() - 1} in our chatting room.")
+        print(f"now we got {threading.activeCount() - 3} in our chatting room.")
 if __name__ == "__main__":
     print("server is started at ", SERVER)
-    start()
-    
+    thread_s = threading.Thread(target=start)
+    thread_s.start()
+    thread_c = threading.Thread(target=control)
+    thread_c.start()
     
