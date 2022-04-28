aec21cc15a006aa021af1e5efc10c8f218e4b841
happy <happyhaha@g.ncu.edu.tw>
2022-04-28 17:06:47 +0800

    server暫時完成

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket, threading, pickle, time, sending
 from winreg import HKEY_LOCAL_MACHINE
+
+from tkinter import *
 # 功能可以接收訊息(基本)，之後要可以進型別的處理(爬蟲、寄信等等)
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -11,16 +13,17 @@ server.bind(ADDR)
 FORMAT = "utf-8-sig"
 
 peoples_in_room = []
+nameandconn = []
+record = ""
+
+
 def announce(conn):
     peoples_in_room.append(conn)
 def kickout(conn):
     for x in peoples_in_room:
-        print(f"{x} : {conn}")
         if x == conn:
             peoples_in_room.remove(x)
-            print(f"remove {x}")
             break
-    print(peoples_in_room)
 
 def broadcast(message):
     data = message.encode(FORMAT)
@@ -30,10 +33,12 @@ def do_email(conn):
     data = b""
     data = conn.recv(4096)
     data = pickle.loads(data)
-    print(data)
     sending.send_mail(data[0], data[1], data[2], data[3])
 def handle_msg(conn, addr):
-    print(f"[NEW CONNECTIONS] {addr} connected")
+    wtt(f"[NEW CONNECTIONS] {addr} connected")
+    data = conn.recv(1024).decode(FORMAT)
+    nameandconn.append((data[:-1], conn))
+    wtt("welcome "+data[:-1])
     connected = True
     while connected:
         data = conn.recv(1024).decode(FORMAT)
@@ -43,28 +48,16 @@ def handle_msg(conn, addr):
             conn.close()
             break
         elif data[-4:] == "iwse":
-            print(f"start doing {data}")
+            wtt(f"start doing {data}")
             thread = threading.Thread(target=do_email, args = (conn,))
             thread.start()
             thread.join()
         else:
             broadcast(data)
-            print(f"{data}")
-    print(f"DISCONNECTED!!")
-def control():
-    while True:
-        message = input("輸入指令")
-        if message == "shutdownclient" and len(peoples_in_room) != 0:
-            broadcast("now shutting all client down")
-            time.sleep(2)
-            print(f"server and clients shutdown")
-        elif len(peoples_in_room) == 0:
-            print("現在沒人")
-        elif message == "listall":
-            for people in peoples_in_room:
-                print(f"{people}")
-        elif message == "shutdownserver":
-            server.close()
+            wtt(data)
+    wtt("DISCONNECTED")
+def wtt(message):
+    text.insert(END, message + "\n")
 def start():
     server.listen()
     while True:
@@ -73,11 +66,28 @@ def start():
         thread.start()
         thread1 = threading.Thread(target = handle_msg, args = (conn, addr))
         thread1.start()
-        print(f"now we got {threading.activeCount() - 3} in our chatting room.")
+        wtt(f"now we got {threading.activeCount() - 2} in our chatting room.")
+def pcm():
+    record = text.get("1.0","end")
+    with open("chatting_record.txt","w+") as fp:
+        fp.write(record)
+        for x in nameandconn:
+            fp.write(str(x)+"\n")
 if __name__ == "__main__":
-    print("server is started at ", SERVER)
+    window = Tk()
+    window.title("Server_control")
+    window.resizable(False, False)
+    window.geometry("570x400")
+    btn = Button(window, text = "print_chat_message",justify=CENTER, command = pcm).pack(side = "top")
+    text = Text(window,height=2, width = 77)
+    text.pack(side = "left",fill=Y)
+    scrollbar = Scrollbar(window)
+    scrollbar.config(command=text.yview)
+    text.config(yscrollcommand=scrollbar.set)
+    scrollbar.pack(side = "right", fill=Y)
+    x = StringVar()
+    wtt("server is started at " + str(SERVER))
     thread_s = threading.Thread(target=start)
     thread_s.start()
-    thread_c = threading.Thread(target=control)
-    thread_c.start()
+    window.mainloop()
     
